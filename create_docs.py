fdcd74b5b3032f43adb5f0621cd022fd7eb418ec
Jeremy <jeraguilon@gmail.com>
2019-01-18 22:53:03 -0500

    Create api docs

--- create_docs.py
+++ create_docs.py
@@ -8,6 +8,8 @@ from run import app
 #subprocess.run(['swagger2markdown', '-i', 'docs/api_docs.json', '-o', 'docs/api_docs.md'])
 swagger_docs = app.test_client().get('/api/spec.json').json
 with open('docs/api_docs.json', 'w') as fp:
-    swagger_docs['info'] = 'Documentation for smart lock'
     json.dump(swagger_docs, fp)
+subprocess.run('api-spec-converter --from=swagger_1 --to=swagger_2 --syntax=json docs/api_docs.json > docs/api_docs_v2.json', shell=True)
+subprocess.run('swagger-markdown -i docs/api_docs_v2.json -o docs/api_docs.md', shell=True)
 
+print("API Docs Updated in docs/api_docs.md")

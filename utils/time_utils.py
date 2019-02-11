acbf1fbb1f534aca66fe6ce71377d71a40c7d056
Jeremy Aguilon <jeraguilon@gmail.com>
2019-02-11 12:02:49 -0500

    Fix merge issue

--- utils/time_utils.py
+++ utils/time_utils.py
@@ -1,6 +1,5 @@
 import datetime
 
-<<<<<<< HEAD
 import pytz
 
 
@@ -9,26 +8,12 @@ def get_current_datetime(timezone: pytz.timezone = None):
         return datetime.datetime.now(timezone)
     else:
         return datetime.datetime.utcnow()
-=======
-from pytz import timezone
-
-
-def get_current_datetime():
-    return datetime.datetime.utcnow()
->>>>>>> 0f15754c8d897d4b88c90e712a0ce1d970fd842c
 
 
 def get_current_time_ms():
     return int(get_current_datetime().strftime("%s")) * 1000
 
 
-<<<<<<< HEAD
 def get_current_day(timezone=pytz.timezone("US/Eastern")):
     local_dt = get_current_datetime(timezone)
-=======
-def get_current_day(timezone=timezone("US/Eastern")):
-    local_dt = timezone.localize(
-        get_current_datetime()
-    )
->>>>>>> 0f15754c8d897d4b88c90e712a0ce1d970fd842c
     return local_dt.strftime("%A").upper()
